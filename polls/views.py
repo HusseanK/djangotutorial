a609dd930b071e0c1c70d0f8b21426f3a65e1e86
Hussean Kalache <husseankalache@hotmail.com>
2025-06-08 19:53:35 +1000

    Minor Updates

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,55 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.db.models import F
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 
-def index(request):
-    return HttpResponse(f"Hello World. You're at the polls index")
+from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
+        return question_list
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    detail_template = "polls/detail.html"
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            detail_template,
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
