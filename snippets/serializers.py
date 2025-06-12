1a464c4d6f6a40e334f8a5cc7f743195dfd40981
Hussean Kalache <husseankalache@hotmail.com>
2025-06-12 18:41:31 +1000

    DRF Pt4 tutorial complete

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,8 +1,31 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
-from snippets.models import Snippet
+
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    language = serializers.ChoiceField(
+        choices=LANGUAGE_CHOICES,
+        initial="python",
+    )
+    style = serializers.ChoiceField(
+        choices=STYLE_CHOICES,
+        initial="friendly",
+    )
+
     class Meta:
         model = Snippet
-        fields = ["id", "title", "code", "linenos", "language", "style"]
+        fields = ["id", "title", "code", "linenos", "language", "style", "owner"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all()
+    )
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "snippets"]
