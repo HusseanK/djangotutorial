6814c54a31ce21fd579e23f1b282719d1329d7e5
Hussean Kalache <husseankalache@hotmail.com>
2025-06-12 11:59:46 +1000

    Serializer Done Pt2 DRF Start

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path("notes/", include("notes.urls")),
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path('', include('snippets.urls')),
 ]
